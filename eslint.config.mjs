e7f724de13855a9f3f062b58327c4c8bee02d044
umidjon11111 <umidjongafforov844@gmail.com>
2026-03-09 00:29:27 +0500

    upt

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -21,6 +21,7 @@ const eslintConfig = [
     ],
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
+      "@next/next/no-img-element": "off", // 👈 disable img warning
     },
   },
 ];
